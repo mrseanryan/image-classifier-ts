2260a439a7888e9e9d0f2ee22d767882e8420f10
str <str_ie@yahoo.co.uk>
2018-12-02 16:50:37 +0100

    build(tslint): Add tslint rules and fix.

--- src/image-classifier-ts.ts
+++ src/image-classifier-ts.ts
@@ -1,4 +1,5 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
 // import "core-js/fn/array.find"
 // ...
+// tslint:disable-next-line:no-unnecessary-class
 export default class DummyClass {}
