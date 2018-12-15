1647c3e6c5656f12b900023f35517beb40b4863d
str <str_ie@yahoo.co.uk>
2018-12-15 23:45:06 +0100

    fix(dependencies): Try to bundle remaining dependencies so not grow install size.

--- src/image-classifier-ts.ts
+++ src/image-classifier-ts.ts
@@ -1,11 +1,21 @@
-// To ensure dependencies are included, without adding them to 'dependencies' which bloats the package, install:
+// Reference files here, To ensure dependencies are included.
+// Else have to add them to 'dependencies' which bloats the package, install size.
 
-// note: cannot export from sharp + rollup chokes on it (C++ ?)
-// - resolved by making sharp external
+// note: cannot export from 'sharp' AND rollup chokes on it (C++ ?)
+// - resolved by making sharp external, adding to dependencies
 
 // note: error importing from @google-cloud/vision
 // - NOT resolved by making it external
 // export * from "./DirectoryProcessor";
+
+// To include exifreader, xmldom
+export * from "./utils/ExifUtils";
+
+// To include image-size
+export * from "./utils/ImageDimensions";
+
+// To include node-fetch
+export * from "./geoCode/GeoCoder";
 
 // TODO xxx remove
 // tslint:disable-next-line:no-unnecessary-class
