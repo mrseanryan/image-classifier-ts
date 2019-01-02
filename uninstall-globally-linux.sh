6573abb790a46f55832a9b79058c5cec68991cdc
sean <str_ie@yahoo.co.uk>
2019-01-02 10:56:22 +0100

    build: Add script to uninstall globally

---
+++ uninstall-globally-linux.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+PACKAGE_NAME=image-classifier-ts
+
+echo Removing $PACKAGE_NAME ...
+
+rm -rf /usr/local/lib/node_modules/$PACKAGE_NAME
+
+rm /usr/local/bin/$PACKAGE_NAME
+
+
+
+
