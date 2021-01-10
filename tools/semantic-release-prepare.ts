8436b448069bee9f0276439f535ab7b721c2b43c
str <str_ie@yahoo.co.uk>
2021-01-10 17:48:43 +0100

    build: fix commit message linting

--- tools/semantic-release-prepare.ts
+++ tools/semantic-release-prepare.ts
@@ -6,7 +6,7 @@ const { readFileSync, writeFileSync } = require("fs");
 const pkg = JSON.parse(readFileSync(path.resolve(__dirname, "..", "package.json")));
 
 pkg.scripts.prepush = "npm run test:prod";
-pkg.scripts.commitmsg = "commitlint -E GIT_PARAMS";
+pkg.scripts.commitmsg = "commitlint -e";
 
 writeFileSync(path.resolve(__dirname, "..", "package.json"), JSON.stringify(pkg, null, 2));
 
