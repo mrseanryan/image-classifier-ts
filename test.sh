020584632d659203bc98590479172cdfbee34f19
str <str_ie@yahoo.co.uk>
2021-01-10 17:39:39 +0100

    chore: Up most of the DEV dependencies

--- test.sh
+++ test.sh
@@ -5,3 +5,5 @@ TMP=`mktemp -d 2>/dev/null || mktemp -d -t 'temp'`
 yarn build:minimal && ./go.sh "static/testData/single" $TMP/image-classifer-ts -replaceOnMove
 
 ./restoreTestData.sh
+
+yarn run test
